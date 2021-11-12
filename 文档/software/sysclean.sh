066f974ffd3563d855045bb6bf501eb5c1bf46e2
Hz² <710921+hz2@users.noreply.github.com>
2021-11-12 17:03:17 +0800

    Create sysclean.sh

---
+++ 文档/software/sysclean.sh
@@ -0,0 +1,29 @@
+
+#!/bin/sh
+
+df -h /
+
+echo 清理日志
+rm -rf /var/log/user.log
+rm -rf /var/log/messages
+rm -rf /var/log/daemon.log
+rm -rf /var/log/syslog*
+
+echo 自动清理 apt
+apt autoremove 
+apt autoclean
+
+echo 清理 apt 缓存
+rm -rf /var/cache/apt
+
+echo 清理 snap 缓存
+rm -rf /var/lib/snapd/cache
+
+echo 清理 flatpak 临时文件
+rm -rf /var/tmp/flatpak-*
+
+echo 清理 docker
+docker system prune -f
+
+df -h /
+
