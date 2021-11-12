b7f26436a4fb52bff781de731dc9bf39f83ba35d
Hz² <710921+hz2@users.noreply.github.com>
2021-11-12 17:27:20 +0800

    Update sysclean.sh

--- 文档/software/sysclean.sh
+++ 文档/software/sysclean.sh
@@ -14,15 +14,24 @@ apt autoremove
 apt autoclean
 
 echo 清理 apt 缓存
+du -sh /var/cache/apt
 rm -rf /var/cache/apt
 
 echo 清理 snap 缓存
+du -sh /var/lib/snapd/cache
 rm -rf /var/lib/snapd/cache
 
 echo 清理 flatpak 临时文件
+du -sh /var/tmp/flatpak-*
 rm -rf /var/tmp/flatpak-*
 
 echo 清理 docker
+docker system df
 docker system prune -f
+docker system df
+
+echo 清理 npm
+npm cache verify
+
 
 df -h / /home
