cdce80d4c19192527e7d605da8d29731c5e0de64
h2 <ht@live.se>
2021-12-29 23:46:13 +0800

    update

---
+++ 文档/software/nightly/link.sh
@@ -0,0 +1,10 @@
+!#/bin/sh
+ln -s /home/a/文档/nightly/firefox/firefox /usr/bin/nightly
+
+cp /home/a/文档/nightly/nightly.desktop /usr/share/applications/
+
+
+
+/home/a/.mozilla/firefox
+installs.ini
+profiles.ini
