e46b1ae7ea95392c9d6731f39b7ef92e177bbef8
h2 <ht@live.se>
2022-01-08 23:38:09 +0800

    update shell

--- 文档/software/pycharm/start_pycharm.sh
+++ 文档/software/pycharm/start_pycharm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 dir=$(readlink $0)
 cd $(dirname $dir)
-sed -Ei 's+(# idea.(\w*).path=.*)+\1\n  idea.\2.path=~/.jetBrains/pycharm/\2+g' ./bin/idea.properties
+if ! grep -q '~/.jetBrains' ./bin/idea.properties; then
+    sed -Ei 's+(# idea.(\w*).path=.*)+\1\n  idea.\2.path=~/.jetBrains/pycharm/\2+g' ./bin/idea.properties
+fi
 
 ./bin/pycharm.sh
