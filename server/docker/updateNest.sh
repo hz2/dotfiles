d1d5a95c21d29d97fe4001fa16d49b7b6058f110
hz2 <710921+hz2@users.noreply.github.com>
2022-02-22 22:57:45 +0800

    Update updateNest.sh

--- server/docker/updateNest.sh
+++ server/docker/updateNest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker system prune -f
-reg=registry.gitlab.com/hzsq/project-nest
+reg=ghcr.io/hz2/project-nest:master
 docker rm -f some-nest
 docker pull ${reg}
 docker run --name some-nest --network="host" -d ${reg}
