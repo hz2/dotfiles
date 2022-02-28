01c5f9fcbcb45065d06b920d99327279c0da4815
h2 <ht@live.se>
2022-02-28 20:50:49 +0800

    update

--- server/docker/arm2/logNest.sh
+++ server/docker/arm2/logNest.sh
@@ -2,6 +2,7 @@
 
 docker system prune -f
 reg=ghcr.io/hz2/project-nest:master
+# reg=registry.gitlab.com/hzsq/project-nest:latest
 docker rm -f some-nest
 docker pull ${reg}
 docker run --name some-nest --network="host" ${reg}
