54c11f0a91e2c754b3050ef136b03156e01f5d26
hz2 <710921+hz2@users.noreply.github.com>
2022-06-29 17:18:02 +0800

    Update updateMemo.sh

--- server/docker/qcloud/updateMemo.sh
+++ server/docker/qcloud/updateMemo.sh
@@ -4,4 +4,4 @@
 docker system prune -f
 docker rm -f the-memo
 docker pull ccr.ccs.tencentyun.com/hzsq/loving-memo:latest
-docker run -d --name some-cn -p 8520:80 ccr.ccs.tencentyun.com/hzsq/loving-memo:latest
+docker run -d --name the-memo -p 8520:80 ccr.ccs.tencentyun.com/hzsq/loving-memo:latest
