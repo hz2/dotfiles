7d84932341bfd848ac56f6f4bcbac0d9bd613635
hz <ht@live.se>
2023-10-25 15:19:14 +0800

    update

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@
 
 echo 'make link '
 
-rm -rf ~/.bash_aliases  ~/.zshrc  ~/.vim  ~/.vimrc 
+mkdir -p ~/.dotfiles_backup/ 
+cp  ~/.* ~/.dotfiles_backup/ 
 
-# ./文档/software/make_link.sh 
-# sudo ln -s $(pwd)/path/* /usr/local/bin/
+# ./software/make_link.sh 
 
-ln -s $(pwd)/.bash_aliases  ~/.bash_aliases
-ln -s $(pwd)/.zshrc  ~/.zshrc
-ln -s $(pwd)/.vim  ~/.vim
-ln -s $(pwd)/.vimrc  ~/.vimrc
+ln -sfn $(pwd)/.bash_aliases  ~/.bash_aliases
+ln -sfn $(pwd)/.bashrc  ~/.bashrc
+ln -sfn $(pwd)/.zshrc  ~/.zshrc
+ln -sfn $(pwd)/.vim  ~/.vim
+ln -sfn $(pwd)/.vimrc  ~/.vimrc
+
 
 echo 'make path'
 echo export PATH=$PATH:$(pwd)/path >> ~/.profile
@@ -19,13 +21,3 @@ sudo echo export PATH=$PATH:$(pwd)/path >> /root/.profile
 
 
 ln -s ~/dotfiles/.config/fish/config.fish  ~/.config/fish/config.sh
-
-# rm -rf  '/usr/local/bin/aptkey'
-# rm -rf  '/usr/local/bin/codium'
-# rm -rf  '/usr/local/bin/idea'
-# rm -rf  '/usr/local/bin/nightly'
-# rm -rf  '/usr/local/bin/proxyoff'
-# rm -rf  '/usr/local/bin/proxyon'
-# rm -rf  '/usr/local/bin/pycharm'
-# rm -rf  '/usr/local/bin/sysclean'
-# rm -rf  '/usr/local/bin/telegram'
--- software/make_link.sh
+++ software/make_link.sh
@@ -3,29 +3,30 @@
 bin=~/.local/bin
 pic=~/.local/share/icons
 app=~/.local/share/applications
+software=${HOME}/software
 mkdir -p $bin
 
 cd $(dirname $0)
 
 # nightly
 
-ln -sfn ${HOME}/software/nightly/firefox/firefox ${bin}/nightly
+ln -sfn ${software}/nightly/firefox/firefox ${bin}/nightly
 ln -sfn $(pwd)/nightly.desktop ${app}
 ln -sfn $(pwd)/nightly/firefox_nightly2.svg ${pic}/nightly.svg
 
 # Telegram
-ln -sfn ${HOME}/software/tgdesktop/Telegram ${bin}/telegram
+ln -sfn ${software}/tgdesktop/Telegram ${bin}/telegram
 ln -sfn $(pwd)/telegram.desktop  ${app}
 
 # vs codium
-ln -sfn ${HOME}/software/codium/bin/codium ${bin}/codium
+ln -sfn ${software}/codium/bin/codium ${bin}/codium
 ln -sfn $(pwd)/codium.desktop  ${app}
 ln -sfn $(pwd)/codium-url-handler.desktop  ${app}
 
 # idea
-ln -sfn $(pwd)/idea-IC/bin/idea.sh ${bin}/idea
+ln -sfn ${software}/idea-IC/bin/idea.sh ${bin}/idea
 ln -sfn $(pwd)/idea.desktop  ${app}
 
 # pycharm
-ln -sfn $(pwd)/pycharm/bin/pycharm.sh ${bin}/pycharm
+ln -sfn ${software}/pycharm/bin/pycharm.sh ${bin}/pycharm
 ln -sfn $(pwd)/pycharm.desktop  ${app}
